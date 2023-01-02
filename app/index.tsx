f9d7a15ba8977208b762e35eb3d8c6bb0881fa58
sadra <sadrataghavi1383@gmail.com>
2023-01-02 23:15:37 +0330

    fix(index): index file update

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,17 @@
 import React, { FC } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import App from './App'
+
 const Root: FC = () => {
-    return <>ROOT</>
+    return (
+        <Router>
+            <App />
+        </Router>
+    )
 }
 
-createRoot(document.getElementById('root')!).render(<Root />)
+const container = document.getElementById('root')!
+createRoot(container).render(<Root />)
