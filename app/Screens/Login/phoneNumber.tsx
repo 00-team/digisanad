23f8bd9ce6b436193025a00b56fb3af5f27eef12
sadra <sadrataghavi1383@gmail.com>
2023-01-04 22:58:51 +0330

    feat(login)

--- app/Screens/Login/phoneNumber.tsx
+++ app/Screens/Login/phoneNumber.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { FC } from 'react'
 
-const PhoneNumber = () => {
+const PhoneNumber: FC = () => {
     return (
         <div className='input-wrapper title'>
             <div className='placeholder'>
@@ -9,15 +9,29 @@ const PhoneNumber = () => {
                 </div>
                 <div className='holder'>شماره تلفن </div>
             </div>
-            <input
-                autoComplete='new-password'
-                className='input title_smaller'
-                name='phonenumber'
-                autoFocus
-                type={'tel'}
-                id='phonenumber'
-                placeholder='09121111111'
-            />
+            <div className='input-column'>
+                <input
+                    autoComplete='new-password'
+                    className='input title_small'
+                    name='phonenumber'
+                    autoFocus
+                    inputMode='numeric'
+                    id='phonenumber'
+                    placeholder='09121111111'
+                />
+                <div
+                    className='icon'
+                    onClick={() => {
+                        const input = document.querySelector(
+                            'input#phonenumber'
+                        ) as HTMLInputElement
+
+                        return (input.value = '')
+                    }}
+                >
+                    <Close />
+                </div>
+            </div>
         </div>
     )
 }
@@ -35,5 +49,19 @@ const Phone = () => (
         xmlns='http://www.w3.org/2000/svg'
     >
         <path d='M256 32C114.6 32 0 125.1 0 240c0 49.6 21.4 95 57 130.7C44.5 421.1 2.7 466 2.2 466.5c-2.2 2.3-2.8 5.7-1.5 8.7 1.3 3 4.1 4.8 7.3 4.8 66.3 0 116-31.8 140.6-51.4 32.7 12.3 69 19.4 107.4 19.4 141.4 0 256-93.1 256-208S397.4 32 256 32zM128.2 304H116c-4.4 0-8-3.6-8-8v-16c0-4.4 3.6-8 8-8h12.3c6 0 10.4-3.5 10.4-6.6 0-1.3-.8-2.7-2.1-3.8l-21.9-18.8c-8.5-7.2-13.3-17.5-13.3-28.1 0-21.3 19-38.6 42.4-38.6H156c4.4 0 8 3.6 8 8v16c0 4.4-3.6 8-8 8h-12.3c-6 0-10.4 3.5-10.4 6.6 0 1.3.8 2.7 2.1 3.8l21.9 18.8c8.5 7.2 13.3 17.5 13.3 28.1.1 21.3-19 38.6-42.4 38.6zm191.8-8c0 4.4-3.6 8-8 8h-16c-4.4 0-8-3.6-8-8v-68.2l-24.8 55.8c-2.9 5.9-11.4 5.9-14.3 0L224 227.8V296c0 4.4-3.6 8-8 8h-16c-4.4 0-8-3.6-8-8V192c0-8.8 7.2-16 16-16h16c6.1 0 11.6 3.4 14.3 8.8l17.7 35.4 17.7-35.4c2.7-5.4 8.3-8.8 14.3-8.8h16c8.8 0 16 7.2 16 16v104zm48.3 8H356c-4.4 0-8-3.6-8-8v-16c0-4.4 3.6-8 8-8h12.3c6 0 10.4-3.5 10.4-6.6 0-1.3-.8-2.7-2.1-3.8l-21.9-18.8c-8.5-7.2-13.3-17.5-13.3-28.1 0-21.3 19-38.6 42.4-38.6H396c4.4 0 8 3.6 8 8v16c0 4.4-3.6 8-8 8h-12.3c-6 0-10.4 3.5-10.4 6.6 0 1.3.8 2.7 2.1 3.8l21.9 18.8c8.5 7.2 13.3 17.5 13.3 28.1.1 21.3-18.9 38.6-42.3 38.6z'></path>
+    </svg>
+)
+
+const Close = () => (
+    <svg
+        stroke='var(--ten-percent)'
+        fill='var(--ten-percent)'
+        stroke-width='0'
+        viewBox='0 0 512 512'
+        height={20}
+        width={20}
+        xmlns='http://www.w3.org/2000/svg'
+    >
+        <path d='M256 48C141.1 48 48 141.1 48 256s93.1 208 208 208 208-93.1 208-208S370.9 48 256 48zm52.7 283.3L256 278.6l-52.7 52.7c-6.2 6.2-16.4 6.2-22.6 0-3.1-3.1-4.7-7.2-4.7-11.3 0-4.1 1.6-8.2 4.7-11.3l52.7-52.7-52.7-52.7c-3.1-3.1-4.7-7.2-4.7-11.3 0-4.1 1.6-8.2 4.7-11.3 6.2-6.2 16.4-6.2 22.6 0l52.7 52.7 52.7-52.7c6.2-6.2 16.4-6.2 22.6 0 6.2 6.2 6.2 16.4 0 22.6L278.6 256l52.7 52.7c6.2 6.2 6.2 16.4 0 22.6-6.2 6.3-16.4 6.3-22.6 0z'></path>
     </svg>
 )
