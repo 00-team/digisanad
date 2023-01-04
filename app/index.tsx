60d2e3ec921f120e88dfa927daeb0474914a8e63
sadra <sadrataghavi1383@gmail.com>
2023-01-04 14:13:50 +0330

    fix(app): alert provider

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,26 @@
 import React, { FC } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import { Options, Provider as AlertProvider } from '@00-team/react-alert'
 import { BrowserRouter as Router } from 'react-router-dom'
+
+import { Alert } from 'components/Alert'
 
 import App from './App'
 
+const AlertOptions: Options = {
+    position: 'top-right',
+    wrapper: () => ({ className: 'wrapper' }),
+    inner: () => ({ className: 'inner' }),
+}
+
 const Root: FC = () => {
     return (
-        <Router>
-            <App />
-        </Router>
+        <AlertProvider template={Alert} options={AlertOptions}>
+            <Router>
+                <App />
+            </Router>
+        </AlertProvider>
     )
 }
 
