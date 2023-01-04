137a63c94e2d87444c488c5aa8ccbd3ebbbe83f5
sadra <sadrataghavi1383@gmail.com>
2023-01-04 13:46:21 +0330

    fix(app): submit btn

---
+++ app/components/common/Submit.tsx
@@ -0,0 +1,43 @@
+import React, { FC, useState } from 'react'
+
+import './style/submit.scss'
+
+interface SubmitProps {
+    title: string
+}
+
+const Submit: FC<SubmitProps> = ({ title }) => {
+    const [Transform, setTransform] = useState({ x: 0, y: 0 })
+    return (
+        <button type='submit' className='see-more-btn-wrapper title_small'>
+            <div
+                className='button-wrapper'
+                onMouseEnter={e => {
+                    const offset = e.currentTarget.getBoundingClientRect()
+                    const relX = e.pageX - offset.left
+                    const relY = e.pageY - offset.top
+
+                    setTransform({ x: relX, y: relY })
+                }}
+                onMouseOut={e => {
+                    const offset = e.currentTarget.getBoundingClientRect()
+                    const relX = e.pageX - offset.left
+                    const relY = e.pageY - offset.top
+
+                    setTransform({ x: relX, y: relY })
+                }}
+            >
+                <div className='text'>{title} </div>
+                <div
+                    className='dot'
+                    style={{
+                        top: `${Transform.y}px`,
+                        left: `${Transform.x}px`,
+                    }}
+                ></div>
+            </div>
+        </button>
+    )
+}
+
+export { Submit }
