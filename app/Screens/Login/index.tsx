f430a7c8954684c457dde42d2b1c3071ce485b16
sadra <sadrataghavi1383@gmail.com>
2023-01-04 13:47:50 +0330

    fix(login): add login card

--- app/Screens/Login/index.tsx
+++ app/Screens/Login/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Card from './Card'
 import Clouds from './Clouds'
 
 import './style/login.scss'
@@ -8,7 +9,7 @@ const Login = () => {
     return (
         <div className='login'>
             <Clouds />
-            <div className='card'></div>
+            <Card />
         </div>
     )
 }
