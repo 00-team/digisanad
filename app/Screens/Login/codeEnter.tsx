84cc3d8efa8871e471c9d81ee40d0def194ab0a7
sadra <sadrataghavi1383@gmail.com>
2023-01-05 16:02:07 +0330

    feat(login): resend sms

--- app/Screens/Login/codeEnter.tsx
+++ app/Screens/Login/codeEnter.tsx
@@ -1,26 +1,73 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 
-const CodeEnter: FC = () => {
+import { Timer } from 'components/common/Timer'
+
+const NUMBER_DEBUG = '09120974956'
+
+interface CodeEnterProps {
+    setStages: (stage: 'phone' | 'code') => void
+}
+
+const CodeEnter: FC<CodeEnterProps> = ({ setStages }) => {
+    const [CanResend, setCanResend] = useState(false)
+
+    const SendAgain = () => {
+        ReactAlert.info('کد تایید مجددا برای شما پیامک شد.')
+        setCanResend(false)
+        return
+    }
+
     return (
-        <div className='input-wrapper title'>
-            <div className='placeholder'>
-                <div className='icon'>
-                    <SubmitCode />
+        <>
+            <div className='input-wrapper title'>
+                <div className='placeholder'>
+                    <div className='icon'>
+                        <SubmitCode />
+                    </div>
+                    <div className='holder'>کد تایید</div>
                 </div>
-                <div className='holder'>کد تایید</div>
+                <div className='input-column'>
+                    <input
+                        autoComplete='one-time-code'
+                        className='input title_small'
+                        name='code'
+                        inputMode='numeric'
+                        autoFocus
+                        id='code'
+                        placeholder='12345'
+                    />
+                </div>
+                <div className='card-options'>
+                    <div className='phone-number-holder title_smaller'>
+                        کد تایید به شماره {NUMBER_DEBUG} پیامک شد.
+                    </div>
+                    {CanResend ? (
+                        <div
+                            className='title_small can-resend'
+                            onClick={() => SendAgain()}
+                        >
+                            ارسال مجدد
+                        </div>
+                    ) : (
+                        <div className='title_small timer-container cant-resend'>
+                            <Timer
+                                start={3}
+                                cb={() => {
+                                    setCanResend(true)
+                                }}
+                            />
+                            <div>ثانیه تا ارسال مجدد</div>
+                        </div>
+                    )}
+                </div>
             </div>
-            <div className='input-column'>
-                <input
-                    autoComplete='one-time-code'
-                    className='input title_small'
-                    name='code'
-                    inputMode='numeric'
-                    autoFocus
-                    id='code'
-                    placeholder='12345'
-                />
+            <div
+                className='go-back-wrapper icon'
+                onClick={() => setStages('phone')}
+            >
+                <GoBack />
             </div>
-        </div>
+        </>
     )
 }
 
@@ -37,5 +84,20 @@ const SubmitCode = () => (
         xmlns='http://www.w3.org/2000/svg'
     >
         <path d='M249.2 224c-14.2-40.2-55.1-72-100.2-72-57.2 0-101 46.8-101 104s45.8 104 103 104c45.1 0 84.1-31.8 98.2-72H352v64h69.1v-64H464v-64H249.2zm-97.6 66.5c-19 0-34.5-15.5-34.5-34.5s15.5-34.5 34.5-34.5 34.5 15.5 34.5 34.5-15.5 34.5-34.5 34.5z'></path>
+    </svg>
+)
+
+const GoBack = () => (
+    <svg
+        stroke='white'
+        fill='white'
+        strokeWidth='0'
+        viewBox='0 0 24 24'
+        height={30}
+        width={30}
+        xmlns='http://www.w3.org/2000/svg'
+    >
+        <path fill='none' d='M0 0h24v24H0V0z' opacity='.87'></path>
+        <path d='M17.51 3.87L15.73 2.1 5.84 12l9.9 9.9 1.77-1.77L9.38 12l8.13-8.13z'></path>
     </svg>
 )
