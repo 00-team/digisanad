f792feb436320f77038014bbf63e50401dffde1c
sadra <sadrataghavi1383@gmail.com>
2023-01-05 16:37:50 +0330

    fix(hero)

--- app/Screens/Home/HeroSection.tsx
+++ app/Screens/Home/HeroSection.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 import './style/hero.scss'
 
+const HERO_IMG = require('../../static/hero.jpg')
+
 const HeroSection = () => {
-    return <section className='hero-container'>HeroSection</section>
+    return (
+        <section
+            className='hero-container'
+            style={{ backgroundImage: `url(${HERO_IMG})` }}
+        >
+            HeroSection
+        </section>
+    )
 }
 
 export default HeroSection
