1c235c148e30a14d6465ad5a938e313ba9bc3508
sadra <sadrataghavi1383@gmail.com>
2023-01-05 16:27:07 +0330

    feat(home): hero init

---
+++ app/Screens/Home/HeroSection.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './style/hero.scss'
+
+const HeroSection = () => {
+    return <section className='hero-container'>HeroSection</section>
+}
+
+export default HeroSection
