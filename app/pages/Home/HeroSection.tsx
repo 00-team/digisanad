f4862c0c5c73b22591d6663927c37a5ffb1eb0ec
sadra <sadrataghavi1383@gmail.com>
2023-01-06 14:47:57 +0330

    feat(hero)

--- app/pages/Home/HeroSection.tsx
+++ app/pages/Home/HeroSection.tsx
@@ -11,13 +11,39 @@ const HeroSection = () => {
             style={{ backgroundImage: `url(${HERO_IMG})` }}
         >
             <div className='hero-wrapper'>
-                <div className='title logo-text'>Digi Sanad</div>
-                <div className='title_smaller'>
-                    راه حلی برای تنظیم قرار داد های فیزیکی
+                <div className='title_hero logo-text hero-header'>
+                    <span>Digi Sanad</span>
                 </div>
-                <div className='input-container'>
-                    <input type='text' />
+                <div className='title words-wrapper'>
+                    {'راه حلی برای تنظیم قرار داد های فیزیکی'
+                        .split(' ')
+                        .map((word, index) => {
+                            return (
+                                <span
+                                    key={index}
+                                    className='hero-word'
+                                    style={{
+                                        animationDelay: `${
+                                            index * 50 + 5500
+                                        }ms`,
+                                    }}
+                                >
+                                    {word}
+                                </span>
+                            )
+                        })}
                 </div>
+                <form
+                    className='input-container title'
+                    onSubmit={e => e.preventDefault()}
+                >
+                    <input
+                        placeholder='جستجو کنید...'
+                        className='title_small'
+                        type={'search'}
+                    />
+                    <button className='title_small'>جستجو</button>
+                </form>
             </div>
         </section>
     )
