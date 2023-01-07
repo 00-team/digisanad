8d397c19df6e6c5538500095ce75e90d63cf79d1
sadra <sadrataghavi1383@gmail.com>
2023-01-07 22:30:16 +0330

    fix(app)

---
+++ app/@types/modules.d.ts
@@ -0,0 +1,11 @@
+// vids
+declare module '*.mp4'
+declare module '*.webm'
+// imgs
+declare module '*.png'
+declare module '*.jpg'
+
+declare module '*.svg' {
+    const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>
+    export default content
+}
