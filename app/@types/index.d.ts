7fd2c732ffea9f1bb75b55b6d7f2cc8c8b98bd61
sadra <sadrataghavi1383@gmail.com>
2023-01-07 15:54:13 +0330

    fix(app): types update

--- app/@types/index.d.ts
+++ app/@types/index.d.ts
@@ -1,5 +1,9 @@
+import { FC, HTMLAttributes } from 'react'
+
 import { AlertContextModel } from '@00-team/react-alert'
 
 declare global {
     var ReactAlert: AlertContextModel
+
+    type Icon = FC<HTMLAttributes<SVGSVGElement>>
 }
