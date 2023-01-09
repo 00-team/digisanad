e4c172e0872882b42cc224a937f65f5651d6e2f3
sadra <sadrataghavi1383@gmail.com>
2023-01-09 15:17:43 +0330

    fix(dashboard): transaction svg

--- app/Icons/Dashboard/Transaction.tsx
+++ app/Icons/Dashboard/Transaction.tsx
@@ -13,7 +13,7 @@ export const TransactionSvg: FC = () => (
         <path
             fill='none'
             stroke='#000'
-            stroke-width='2'
+            strokeWidth='2'
             d='M2,7 L20,7 M16,2 L21,7 L16,12 M22,17 L4,17 M8,12 L3,17 L8,22'
         ></path>
     </svg>
