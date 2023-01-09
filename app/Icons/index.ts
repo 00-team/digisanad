38e847439324e5df4eb8670bffdad2c682b2e877
sadra <sadrataghavi1383@gmail.com>
2023-01-09 15:17:22 +0330

    refactor(app): icons

--- app/Icons/index.ts
+++ app/Icons/index.ts
@@ -1,1 +1,6 @@
 export * from './Actions/Edit'
+export * from './Actions/Send'
+export * from './Dashboard/Contract'
+export * from './Dashboard/Globe'
+export * from './Dashboard/Transaction'
+export * from './Models/Person'
