2c91520d8cdcc297062799dec12a602b6c5dd559
sadra <sadrataghavi1383@gmail.com>
2023-01-09 16:59:45 +0330

    fix(dashboard): my info section update

--- app/pages/Dashboard2/MyInfo.tsx
+++ app/pages/Dashboard2/MyInfo.tsx
@@ -1,7 +1,52 @@
 import React from 'react'
 
+import './style/myinfo.scss'
+
+const contractSvg = require('../../static/Dashboard/myInfo/contract.svg')
+const transactionSvg = require('../../static/Dashboard/myInfo/transaction.svg')
+
 const MyInfo = () => {
-    return <div>MyInfo</div>
+    return (
+        <section className='myinfo-container'>
+            <div className='section-header section_title'>
+                <span>اطلاعات من</span>
+            </div>
+
+            <div className='myinfo-columns'>
+                <div className='myinfo-column'>
+                    <object
+                        className='svg-container'
+                        data={transactionSvg}
+                    ></object>
+                    <div className='content title_hero'>
+                        <div className='holder'> تراکنش های من </div>
+                        <div className='data'>123</div>
+                    </div>
+                </div>
+                <div className='myinfo-column'>
+                    <object
+                        className='svg-container'
+                        data={contractSvg}
+                    ></object>
+                    <div className='content title_hero'>
+                        <div className='holder'>قرارداد های من</div>
+                        <div className='data'>123</div>
+                    </div>
+                </div>
+
+                <div className='myinfo-column'>
+                    <object
+                        className='svg-container'
+                        data={transactionSvg}
+                    ></object>
+                    <div className='content title_hero'>
+                        <div className='holder'> تراکنش های من </div>
+                        <div className='data'>123</div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default MyInfo
