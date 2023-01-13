36189afa3c0c3d98dd7d2274be55fc80ce7ea02d
sadra <sadrataghavi1383@gmail.com>
2023-01-13 15:28:48 +0330

    fix(app): icon

---
+++ app/Icons/Models/GoBack.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const GoBack = () => {
+    return (
+        <svg
+            stroke='white'
+            fill='white'
+            strokeWidth='0'
+            viewBox='0 0 24 24'
+            height={30}
+            width={30}
+            xmlns='http://www.w3.org/2000/svg'
+        >
+            <path fill='none' d='M0 0h24v24H0V0z' opacity='.87'></path>
+            <path d='M17.51 3.87L15.73 2.1 5.84 12l9.9 9.9 1.77-1.77L9.38 12l8.13-8.13z'></path>
+        </svg>
+    )
+}
