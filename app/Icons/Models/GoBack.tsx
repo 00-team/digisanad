825f7451c30bbb845db60f8ae26c08301fb05f85
sadra <sadrataghavi1383@gmail.com>
2023-01-13 22:14:49 +0330

    fix(app)

--- app/Icons/Models/GoBack.tsx
+++ app/Icons/Models/GoBack.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { FC } from 'react'
 
-export const GoBack = () => {
+export const GoBack: FC = () => {
     return (
         <svg
             stroke='white'
