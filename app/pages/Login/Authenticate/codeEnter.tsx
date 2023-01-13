18baf17eefbafd07aa6e91c3ac5ca0b8bc280bde
sadra <sadrataghavi1383@gmail.com>
2023-01-13 22:16:00 +0330

    refactor(login)

--- app/pages/Login/Authenticate/codeEnter.tsx
+++ app/pages/Login/Authenticate/codeEnter.tsx
@@ -6,6 +6,8 @@ import axios from 'axios'
 import { useAtom } from 'jotai'
 import { LoginAtom } from 'state'
 
+import { CodeSvg } from 'Icons/Dashboard/Code'
+
 import { Timer } from 'components/common/Timer'
 
 const NUMBER_DEBUG = '09120974956'
@@ -26,7 +28,7 @@ const CodeEnter: FC = () => {
                 resend: !response.data.timer,
             })
         } else {
-            ReactAlert.error('Error while authenticating!')
+            ReactAlert.error('خطا! لطفا دوباره تلاش کنید.')
         }
     }
 
@@ -35,7 +37,7 @@ const CodeEnter: FC = () => {
             <div className='input-wrapper title'>
                 <div className='placeholder'>
                     <div className='icon'>
-                        <SubmitCode />
+                        <CodeSvg />
                     </div>
                     <div className='holder'>کد تایید</div>
                 </div>
@@ -80,17 +82,3 @@ const CodeEnter: FC = () => {
 }
 
 export default CodeEnter
-
-const SubmitCode = () => (
-    <svg
-        stroke='currentColor'
-        fill='currentColor'
-        strokeWidth='0'
-        viewBox='0 0 512 512'
-        height={20}
-        width={20}
-        xmlns='http://www.w3.org/2000/svg'
-    >
-        <path d='M249.2 224c-14.2-40.2-55.1-72-100.2-72-57.2 0-101 46.8-101 104s45.8 104 103 104c45.1 0 84.1-31.8 98.2-72H352v64h69.1v-64H464v-64H249.2zm-97.6 66.5c-19 0-34.5-15.5-34.5-34.5s15.5-34.5 34.5-34.5 34.5 15.5 34.5 34.5-15.5 34.5-34.5 34.5z'></path>
-    </svg>
-)
