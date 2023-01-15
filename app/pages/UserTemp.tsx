d54fafd7f068454f0a3f0fd564dca334c33574de
sadra <sadrataghavi1383@gmail.com>
2023-01-15 12:52:51 +0330

    fix(userTemp)

--- app/pages/UserTemp.tsx
+++ app/pages/UserTemp.tsx
@@ -11,20 +11,6 @@ const UserTemp: FC = () => {
         setUser('fetch')
     }, [setUser])
 
-    const update = () => {
-        if (!picture.current || !picture.current.files) return
-        const pic = picture.current.files[0]
-        if (!pic) return
-
-        setUser([
-            'update',
-            {
-                picture: pic,
-                nickname: 'سید صدرا تقوی',
-            },
-        ])
-    }
-
     return (
         <div
             style={{
@@ -38,12 +24,9 @@ const UserTemp: FC = () => {
             }}
         >
             {User.user_id === 0 && <span>No User</span>}
-            <span>nickname: {User.nickname}</span>
-            <span>picture: {User.picture}</span>
+            <span>nickname: {User.first_name}</span>
             <span>user_id: {User.user_id}</span>
-            {User.picture && <img src={User.picture} width='200' alt='' />}
             <input type='file' ref={picture} />
-            <button onClick={update}>Update</button>
         </div>
     )
 }
