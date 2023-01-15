303243554a676cfd0647e579e41ddee457c7a395
sadra <sadrataghavi1383@gmail.com>
2023-01-15 14:19:20 +0330

    feat(app): user model update

--- app/state/models/User.ts
+++ app/state/models/User.ts
@@ -1,14 +1,15 @@
 interface User {
-    address: string
-    birth_date: string[]
-    email: string
     first_name: string
     last_name: string
-    national_id: string
     phone: string | null
+    national_id: string
+    birth_date: string[]
+    email: string
+    address: string
     postal_code: string
     token: string | null
     user_id: number | null
+
     wallet: number
 }
 
