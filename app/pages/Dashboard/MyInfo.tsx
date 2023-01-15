a52d9c16bb60b648b8b98d0ffc8c065813e7c4ca
sadra <sadrataghavi1383@gmail.com>
2023-01-15 11:51:56 +0330

    fix(dashboard)

--- app/pages/Dashboard/MyInfo.tsx
+++ app/pages/Dashboard/MyInfo.tsx
@@ -1,24 +1,13 @@
-import React, {
-    ChangeEvent,
-    FC,
-    MutableRefObject,
-    useRef,
-    useState,
-} from 'react'
+import React, { FC, useRef, useState } from 'react'
 
 import { PersonSvg } from 'Icons'
-import { Close } from 'Icons/Actions/Close'
 
 import { useAtom } from 'jotai'
 import { UserAtom } from 'state'
 
-import { Profile } from 'Icons/Dashboard/Profile'
-
 import { Submit } from 'components'
 
 import './style/myinfo.scss'
-
-import DEFAULT_IMG from 'static/avatar.png'
 
 const MyInfo: FC = () => {
     const [User, setUser] = useAtom(UserAtom)
@@ -51,7 +40,6 @@ const MyInfo: FC = () => {
                             />
                         </div>
                     </div>
-                    <Avatar value={picture} picture={User.picture} />
                 </div>
                 <div className='submit-wrapper'>
                     <Submit
@@ -73,65 +61,6 @@ const MyInfo: FC = () => {
                 </div>
             </div>
         </section>
-    )
-}
-
-interface AvatarProps {
-    picture: string | null
-    value: MutableRefObject<File | 'delete' | null>
-}
-
-const Avatar: FC<AvatarProps> = ({ value, picture }) => {
-    const [Image, setImage] = useState(picture || DEFAULT_IMG)
-
-    const ChangeImg = (e: ChangeEvent<HTMLInputElement>) => {
-        if (!e.currentTarget.files || !e.currentTarget.files[0]) return
-
-        const pic = e.currentTarget.files[0]
-        value.current = pic
-
-        setImage(URL.createObjectURL(pic!))
-    }
-
-    return (
-        <div className='avatar row'>
-            <div className='avatar-title title row-title'>
-                <div className='icon'>
-                    <Profile />
-                </div>
-                عکس پروفایل
-            </div>
-            <div className='input-wrapper '>
-                <div className='label-wrapper'>
-                    <label
-                        className='avatar-img'
-                        htmlFor='avatar-img'
-                        style={{
-                            backgroundImage: `url(${Image})`,
-                        }}
-                    ></label>
-                    {Image !== DEFAULT_IMG && (
-                        <label
-                            className='can-clear'
-                            // htmlFor='avatar-img'
-                            onClick={() => {
-                                value.current = 'delete'
-                                setImage(DEFAULT_IMG)
-                            }}
-                        >
-                            <Close />
-                        </label>
-                    )}
-                </div>
-                <input
-                    id='avatar-img'
-                    type='file'
-                    accept='image/x-png,image/jpeg'
-                    multiple={false}
-                    onChange={ChangeImg}
-                />
-            </div>
-        </div>
     )
 }
 
