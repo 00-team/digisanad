9b8f963e96dc705cb3048a3c9d7ff2fd35693065
sadra <sadrataghavi1383@gmail.com>
2023-01-15 12:52:41 +0330

    fix(app): user model atom

--- app/state/atom/user.ts
+++ app/state/atom/user.ts
@@ -5,47 +5,40 @@ import { atomWithStorage } from 'jotai/utils'
 import { UserModel } from 'state'
 
 const DEFUALT_USER: UserModel = {
-    user_id: 0,
-    nickname: null,
-    wallet: 0,
-    picture: null,
-    token: null,
+    address: '',
+    birth_date: [],
+    email: '',
+    first_name: '',
+    last_name: '',
+    national_id: '',
     phone: null,
+    postal_code: '',
+    token: null,
+    user_id: 0,
+    wallet: 0,
 }
 
 const User = atomWithStorage<UserModel>('user', DEFUALT_USER)
 
-interface UpdateArgs {
-    picture?: File | 'delete' | null
-    nickname?: string | null
-}
-type Args = 'clear' | 'fetch' | ['update', UpdateArgs] | Partial<UserModel>
+type Args = 'clear' | 'fetch' | Partial<UserModel>
 
 const UserAtom = atom(
     get => get(User),
     async (get, set, args: Args) => {
         let user = get(User)
 
-        if (Array.isArray(args) && args[0] == 'update') {
-            if (!user.token) return
-            let fd = new FormData()
+        if (args === 'fetch') {
+            try {
+                if (!user.token) return
 
-            if (args[1].nickname) fd.set('nickname', args[1].nickname)
-            if (args[1].picture) fd.set('picture', args[1].picture)
+                let response = await axios.get('/api/user/get/', {
+                    headers: { Authorization: `user ${user.token}` },
+                })
 
-            let response = await axios.post('/api/user/get/', fd, {
-                headers: { Authorization: `user ${user.token}` },
-            })
-
-            console.log('update', response.data)
-            set(User, { ...user, ...response.data })
-        } else if (args === 'fetch') {
-            if (!user.token) return
-            let response = await axios.get('/api/user/get/', {
-                headers: { Authorization: `user ${user.token}` },
-            })
-            console.log('fetch', response.data)
-            set(User, { ...user, ...response.data })
+                set(User, { ...user, ...response.data, token: user.token })
+            } catch (error) {
+                console.log(error)
+            }
         } else if (args === 'clear') {
             set(User, DEFUALT_USER)
         } else {
