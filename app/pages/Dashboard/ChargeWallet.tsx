d887f33cba34c20609712c54e1caa63a9e712715
sadra <sadrataghavi1383@gmail.com>
2023-02-01 15:47:30 +0330

    feat(changewallet): init

---
+++ app/pages/Dashboard/ChargeWallet.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react'
+
+import './style/chargewallet.scss'
+
+const ChargeWallet: FC = () => {
+    return (
+        <section className='changewallet-container'>
+            <div className='section-header section_title'>شارژ کیف پول</div>
+            <div className='changewallet-wrapper'></div>
+        </section>
+    )
+}
+
+export default ChargeWallet
