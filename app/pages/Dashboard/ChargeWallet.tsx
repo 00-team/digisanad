85362f7fd9b622ec58ea55ef6d0c5906d4d1283f
sadra <sadrataghavi1383@gmail.com>
2023-02-01 16:00:21 +0330

    fix(chargeWallet)

--- app/pages/Dashboard/ChargeWallet.tsx
+++ app/pages/Dashboard/ChargeWallet.tsx
@@ -6,7 +6,14 @@ const ChargeWallet: FC = () => {
     return (
         <section className='changewallet-container'>
             <div className='section-header section_title'>شارژ کیف پول</div>
-            <div className='changewallet-wrapper'></div>
+            <div className='changewallet-wrapper'>
+                <div className='supported-banks-wrapper title'>
+                    <div className='banks-header'>
+                        <span>بانک های قابل برداشت</span>
+                    </div>
+                    <div className='banks-slider'></div>
+                </div>
+            </div>
         </section>
     )
 }
