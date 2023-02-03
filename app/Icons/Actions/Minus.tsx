d51add6574c4ba0942b91ea9b07fdf9c5da48d47
sadra <sadrataghavi1383@gmail.com>
2023-02-03 13:44:33 +0330

    fix(dasbhaord): minus icon

---
+++ app/Icons/Actions/Minus.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const MinusSvg = () => {
+    return (
+        <svg
+            stroke='currentColor'
+            fill='currentColor'
+            strokeWidth='0'
+            viewBox='0 0 24 24'
+            height={20}
+            width={20}
+            xmlns='http://www.w3.org/2000/svg'
+        >
+            <path d='M5 11h14v2H5z'></path>
+        </svg>
+    )
+}
