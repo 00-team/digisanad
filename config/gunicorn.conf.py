37d83b603863da06349b8f85fe373f60c7477b38
007 <dr007cc@gmail.com>
2023-05-24 16:54:48 +0330

    fix: config fixes

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -2,7 +2,7 @@
 import multiprocessing
 
 workers = multiprocessing.cpu_count() * 2 + 1
-wsgi_app = 'main:app'
+wsgi_app = '/digisanad/main:app'
 proc_name = 'digisanad gun'
 worker_class = 'uvicorn.workers.UvicornWorker'
 venv = '/digisanad/.env/bin/activate'
