8af67a2922101bf6865824c04cbd8aec7752926f
007 <dr007cc@gmail.com>
2023-05-31 00:20:10 +0330

    feat: add api/verification route

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,23 @@
 from fastapi import APIRouter
 
+from shared.errors import bad_verification
+
 from .modules import auth, user
+from .verification import VerificationResponse, verification
 
 router = APIRouter(
     prefix='/api',
+)
+
+
+router.add_api_route(
+    '/verification/', verification, methods=['POST'],
+    openapi_extra={'errors': [bad_verification]},
+    response_model=VerificationResponse,
+    description=(
+        'send verification for an action that requires code verification<br/>'
+        'like deleteing user account or login'
+    )
 )
 
 
