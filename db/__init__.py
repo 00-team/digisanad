0b2c9e125449696d7735722ae3d73ef3981e0642
007 <dr007cc@gmail.com>
2023-05-31 00:18:58 +0330

    fix: delete old db stuff

--- db/__init__.py
+++ db/__init__.py
@@ -1,25 +1,25 @@
 
 # importing the table is necessary for creating the tables
-from sqlalchemy import create_engine
+# from sqlalchemy import create_engine
 
-import db.table
-from shared import settings
+# import db.table
+# from shared import settings
 
-from .models import TransactionModel, UserModel, VerificationModel
-from .shared import database, metadata, redis
+# from .models import TransactionModel, UserModel, VerificationModel
+# from .shared import database, metadata, redis
+#
+# engine = create_engine(
+#     settings.sql_url,
+#     connect_args={'check_same_thread': False},
+# )
+# metadata.create_all(engine)
 
-engine = create_engine(
-    settings.sql_url,
-    connect_args={'check_same_thread': False},
-)
-metadata.create_all(engine)
 
-
-__all__ = [
-    'database',
-    'redis',
-
-    'VerificationModel',
-    'UserModel',
-    'TransactionModel',
-]
+# __all__ = [
+#     'database',
+#     'redis',
+#
+#     'VerificationModel',
+#     'UserModel',
+#     'TransactionModel',
+# ]
--- db/table/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from .user import transaction_table, user_table
-
-__all__ = [
-    'user_table',
-    'transaction_table',
-]
