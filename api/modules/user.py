834a04bb4f755f07aed666fbf59a8b319c5856cf
007 <dr007cc@gmail.com>
2023-05-31 00:21:11 +0330

    fix: clean users module

--- api/modules/user.py
+++ api/modules/user.py
@@ -1,22 +1,13 @@
 
-import web3
-from fastapi import APIRouter, HTTPException, Request
+from fastapi import APIRouter, Request
 
-from api.models.user import DeleteResponse, DeleteVerifyBody
-from api.models.user import DeleteVerifyResponse
-from db import UserModel
-from db.api.user import user_delete
-from shared import settings
-from shared.tools import now
-from utils import rate_limit, send_verification, user_required
-from utils import verify_verification
-
-PAYMENT_ERROR = HTTPException(500, 'payment error')
+from db.models import UserModel
+from deps import login_required, rate_limit
 
 router = APIRouter(
     prefix='/user',
     tags=['user'],
-    dependencies=[user_required()]
+    dependencies=[login_required(), rate_limit('user', 60, 120)]
 )
 
 
@@ -35,32 +26,3 @@ async def test(request: Request):
     #     'to': '',
     #     'value': 1
     # })
-
-
-@router.post(
-    '/delete/',
-    response_model=DeleteResponse,
-    dependencies=[rate_limit('user_delete', 3660, 20)]
-)
-async def delete(request: Request):
-    user: UserModel = request.state.user
-
-    return await send_verification(user.phone, 'DELETE')
-
-
-@router.post(
-    '/delete_verify/',
-    response_model=DeleteVerifyResponse,
-    dependencies=[rate_limit('user_delete', 3660, 20)]
-)
-async def delete_verify(request: Request, body: DeleteVerifyBody):
-    user: UserModel = request.state.user
-
-    await verify_verification(user.phone, body.code, 'DELETE')
-
-    await user_delete(user.user_id)
-
-    return {
-        'ok': True,
-        'datetime': now()
-    }
