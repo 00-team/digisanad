70bdd12c6166151fb61a746e1976fd756ec60a6a
007 <dr007cc@gmail.com>
2023-05-31 01:00:48 +0330

    fix: update auth deps

--- deps/auth.py
+++ deps/auth.py
@@ -1,97 +1,105 @@
 
 from hashlib import sha3_512, sha256
 
-from fastapi import Depends, HTTPException, Request
+from fastapi import Depends, Request, Response
+from fastapi.security import HTTPBearer
 
-# from database.plutus import User, admin_check_token
-from db.models import UserModel
+from db.models import UserModel, UsersTable
 from db.rate_limit import rate_limit_get, rate_limit_set
+from db.user import user_get
+from shared.errors import bad_auth, forbidden, rate_limited
 
-# from db.api.user import user_get_by_id
+user_schema = HTTPBearer(description='User Token')
 
-INVALID_AUTH = HTTPException(403, 'Invalid authentication credentials')
+errors = [bad_auth, rate_limited]
 
 
-def rate_limit_check(period: int, amount: int):
-    async def check(request, path_id, update):
+def get_ip():
+    '''get the client ip'''
+
+    async def decorator(request: Request):
         forwarded = request.headers.get('X-Forwarded-For')
+
         if forwarded:
             ip = forwarded.split(',')[0]
         else:
             ip = request.client.host
 
-        identifier = sha256(f'{path_id}:{ip}'.encode('utf-8')).hexdigest()
+        request.state.ip = ip
 
-        key = f'token:{identifier}'
-        value, expire = await rate_limit_get(key)
-
-        if value >= amount:
-            raise HTTPException(
-                429, 'Too Many Requests',
-                headers={
-                    'X-RateLimit-Limit': str(amount),
-                    'X-RateLimit-Reset-After': str(expire)
-                }
-            )
-
-        if update:
-            await rate_limit_set(key, period)
-
-    return check
-
-
-rate_limit = rate_limit_check(3600, 10)
-
-
-def get_token(request: Request, schema: str) -> str:
-    authorization = request.headers.get(
-        'Authorization',
-        request.cookies.get('Authorization')
-    )
-
-    if not authorization:
-        raise HTTPException(403, 'Not authenticated')
-
-    token = authorization.partition(' ')
-
-    if len(token) != 3:
-        raise INVALID_AUTH
-
-    if token[0].lower() != schema:
-        raise INVALID_AUTH
-
-    return token[2]
-
-
-def token_id(token_raw: str):
-    try:
-        data_id, token = token_raw.split(':')
-        return int(data_id), token
-    except ValueError:
-        raise INVALID_AUTH
-
-
-def login_required():
-    '''user token is required'''
-
-    async def decorator(request: Request):
-        user = getattr(request.state, 'user', None)
-
-        if isinstance(user, UserModel):
-            return user.user_id
-
-        user_id, token = token_id(get_token(request, 'user'))
-        token_hash = sha3_512(token.encode()).hexdigest()
-
-        user = await user_get_by_id(user_id, token_hash)
-
-        condition = user is None
-        await rate_limit(request, 'user_token_check', condition)
-
-        if condition:
-            raise INVALID_AUTH
-
-        request.state.user = user
-        return user.user_id
+        return ip
 
     return Depends(decorator)
+
+
+async def rate_limit(request, path_id):
+    # TODO: remove this debug code
+    return
+    period = 3600
+    amount = 10
+
+    ip = request.state.ip
+    identifier = sha256(f'{path_id}:{ip}'.encode('utf-8')).hexdigest()
+    key = f'invalid_token:{identifier}'
+
+    value, expire = await rate_limit_get(key)
+
+    if value >= amount:
+        raise rate_limited(headers={
+            'X-RateLimit-Limit': str(amount),
+            'X-RateLimit-Reset-After': str(expire)
+        })
+
+    await rate_limit_set(key, period)
+
+
+def id_token(raw: str):
+    try:
+        identifier, token = raw.split(':')
+        return int(identifier), token
+    except ValueError:
+        raise bad_auth
+
+
+def user_required():
+    '''user token is required'''
+
+    async def decorator(
+        request: Request, response: Response,
+        token=Depends(user_schema)
+    ):
+        state = getattr(request.state, 'user', None)
+
+        if isinstance(state, UserModel):
+            return state
+
+        user_id, token = id_token(token.credentials)
+        user = await user_get(UsersTable.user_id == user_id)
+
+        if user is None:
+            await rate_limit(request, 'user_token_check')
+            raise bad_auth
+
+        if user.token != sha3_512(token.encode()).hexdigest():
+            await rate_limit(request, 'user_token_check')
+            raise bad_auth
+
+        request.state.user = user
+        return user
+
+    dep = Depends(decorator)
+    dep.errors = errors
+    return dep
+
+
+def admin_required():
+    '''admin token is required'''
+
+    async def decorator(request: Request, user: UserModel = user_required()):
+        # if not user.admin or state.agent.admin_perms == 0:
+        #     await rate_limit(request, 'admin_check')
+        raise forbidden
+
+    dep = Depends(decorator)
+    dep.errors = errors + [forbidden]
+    return dep
