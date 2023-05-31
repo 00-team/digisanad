beb463aa014d4a9750ae3ba30e884fb664a77bca
007 <dr007cc@gmail.com>
2023-06-01 01:34:52 +0330

    feat: add update_wallet util

--- shared/crypto.py
+++ shared/crypto.py
@@ -1,7 +1,9 @@
 
 import json
 
+from db.models import WalletModel
 from shared import W3, settings
+from shared.tools import now
 
 
 def get_abi(name: str) -> list:
@@ -23,6 +25,39 @@ ETH_TOKENS = {
 }
 
 
+async def update_wallet(wallet: WalletModel = None) -> WalletModel:
+    if wallet is None:
+        acc = W3.eth.account.create()
+    else:
+        acc = W3.eth.account.from_key(wallet.eth_pk)
+
+    eth_balance = await W3.eth.get_balance(acc.address)
+    tokens = {}
+
+    for k, t in ETH_TOKENS:
+        tokens[k] = await t['contract'].functions.balanceOf(acc.address).call()
+
+    if wallet is None:
+        return WalletModel(
+            wallet_id=0,
+            user_id=0,
+            last_update=now(),
+            eth_pk=acc.key.hex(),
+            eth_addr=acc.address,
+            eth_balance=eth_balance,
+            eth_tokens=tokens
+        )
+
+    wallet.last_update = now()
+    wallet.eth_balance = eth_balance
+    wallet.eth_tokens = tokens
+    wallet.eth_pk = acc.key.hex()
+    wallet.eth_addr = acc.address
+
+    return wallet
+
+
 __all__ = [
-    'ETH_WEI', 'ETH_GWEI', 'ETH_TOKENS'
+    'ETH_WEI', 'ETH_GWEI', 'ETH_TOKENS',
+    'update_wallet'
 ]
