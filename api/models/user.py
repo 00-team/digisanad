25e4f19b22182add05e3b2049dc217c36086afaf
007 <dr007cc@gmail.com>
2023-06-01 02:17:12 +0330

    feat: add user/wallet route

--- api/models/user.py
+++ api/models/user.py
@@ -1,1 +1,13 @@
 
+from pydantic import BaseModel
+
+from db.models import WalletModel
+
+
+class WalletResponse(BaseModel):
+    wallet_id: int
+    user_id: int
+    next_update: int
+    eth_addr: str
+    eth_balance: int
+    eth_tokens: dict
--- api/modules/user.py
+++ api/modules/user.py
@@ -1,8 +1,11 @@
 
 from fastapi import APIRouter, Request
 
-from db.models import UserModel
+from api.models.user import WalletResponse
+from db.models import UserModel, WalletTable
+from db.wallet import wallet_add, wallet_get, wallet_update
 from deps import rate_limit, user_required
+from shared.crypto import update_wallet
 
 router = APIRouter(
     prefix='/user',
@@ -13,16 +16,31 @@ router = APIRouter(
 
 @router.get('/', response_model=UserModel)
 async def get(request: Request):
-    return request.state.user
+    user: UserModel = request.state.user
+    user.token = user.token[:32]
+    return user
 
 
-@router.get('/test/')
-async def test(request: Request):
-    pass
-    # user = request.state.user
-    # ac = web3.Account()
-    # w.eth.send_transaction({
-    #     'from': '---',
-    #     'to': '',
-    #     'value': 1
-    # })
+@router.get('/wallet/', response_model=WalletResponse)
+async def wallet(request: Request):
+    user: UserModel = request.state.user
+    wallet = await wallet_get(WalletTable.user_id == user.user_id)
+
+    if wallet is None or wallet.next_update < 1:
+        new_wallet = await update_wallet(wallet)
+
+        if wallet is None:
+            new_wallet.user_id = user.user_id
+            wallet_id = await wallet_add(**new_wallet.dict())
+            new_wallet.wallet_id = wallet_id
+        else:
+            await wallet_update(
+                WalletTable.wallet_id == wallet.wallet_id,
+                **new_wallet.dict()
+            )
+
+        wallet = new_wallet
+
+    response = wallet.dict()
+    response['next_update'] = wallet.next_update
+    return response
