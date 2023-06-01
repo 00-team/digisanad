2ef3c8dfff9bc5363d8b4d39de7f7eb7ed846d3c
007 <dr007cc@gmail.com>
2023-06-01 13:25:19 +0330

    fix: remove print statement

--- shared/sms.py
+++ shared/sms.py
@@ -7,7 +7,7 @@ BASE_API = 'https://console.melipayamak.com/api'
 
 
 async def send_code_sms(phone: str, code: str):
-    res = httpx.post(
+    httpx.post(
         f'{BASE_API}/send/simple/{settings.meilisms_tokne}',
         json={
             'from': '50004001777036',
@@ -15,4 +15,3 @@ async def send_code_sms(phone: str, code: str):
             'text': f'کد ورود شما به دیجی سند: {code}'
         }
     )
-    print(res.json())
