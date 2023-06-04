33486c7f109a33366cef858ec5df904790712ed4
sadra <sadrataghavi1383@gmail.com>
2023-06-05 00:20:33 +0330

    feat(icons)

--- app/icons/index.ts
+++ app/icons/index.ts
@@ -2,9 +2,26 @@ export * from './Actions/Edit'
 export * from './Actions/Send'
 export * from './Actions/Plus'
 export * from './Actions/Minus'
+
 export * from './Dashboard/Contract'
 export * from './Dashboard/Globe'
 export * from './Dashboard/Transaction'
 export * from './Dashboard/Coin'
+
 export * from './Models/Person'
-export * from './tokens'
+
+export * from './Socials/Instagram'
+export * from './Socials/Telegram'
+export * from './Socials/Whatsapp'
+
+export * from './Footer/Call'
+export * from './Footer/Fax'
+export * from './Footer/Location'
+
+export * from './Tokens/Shiba'
+export * from './Tokens/Tether'
+
+export interface SvgProps {
+    size: number
+    style?: React.CSSProperties
+}
