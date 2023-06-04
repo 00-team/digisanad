4204920016b6c899d88e6c74a890ad3d740d865c
sadra <sadrataghavi1383@gmail.com>
2023-06-04 23:21:10 +0330

    fix(footer)

---
+++ app/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+import './style/footer.scss'
+
+const Footer: FC = () => {
+    return <footer className='footer-container'>Footer</footer>
+}
+
+export default Footer
