b7ee7d0a6c90f8aafabfe03470544b6134185fbb
007 <dr007cc@gmail.com>
2023-07-15 10:58:13 +0330

    fix: dont send sms on debug

--- shared/sms.py
+++ shared/sms.py
@@ -7,6 +7,14 @@ BASE_API = 'https://console.melipayamak.com/api'
 
 
 async def send_code_sms(phone: str, code: str):
+    if settings.debug:
+        print(
+            '--------------------\n'
+            f'phone: {phone}\ncode: {code}\n'
+            '--------------------'
+        )
+        return
+
     httpx.post(
         f'{BASE_API}/send/simple/{settings.meilisms_tokne}',
         json={
