a826425572ba68d6cfad4840c029cc01ef3fcdeb
sadra <sadrataghavi1383@gmail.com>
2023-07-22 15:53:24 +0330

    feat(types)

--- app/@types/index.d.ts
+++ app/@types/index.d.ts
@@ -2,8 +2,12 @@ import { FC, HTMLAttributes } from 'react'
 
 import { AlertContextModel } from '@00-team/react-alert'
 
+interface IconAttrs extends HTMLAttributes<SVGSVGElement> {
+    size?: number | string
+}
+
 declare global {
     var ReactAlert: AlertContextModel
-    type Icon = FC<HTMLAttributes<SVGSVGElement>>
+    type Icon = FC<IconAttrs>
     var HandleError: (error: unknown) => void
 }
