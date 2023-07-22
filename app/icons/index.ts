775bacf16f4ad57dfb286d679ccecb8e3626b635
sadra <sadrataghavi1383@gmail.com>
2023-07-22 16:06:44 +0330

    fix(icons)

--- app/icons/index.ts
+++ app/icons/index.ts
@@ -2,12 +2,14 @@ export * from './Actions/Edit'
 export * from './Actions/Send'
 export * from './Actions/Plus'
 export * from './Actions/Minus'
+export * from './Actions/Close'
 
 export * from './Dashboard/Contract'
 export * from './Dashboard/Globe'
 export * from './Dashboard/Transaction'
 export * from './Dashboard/Coin'
 export * from './Dashboard/ArrowUp'
+export * from './Dashboard/ArrowDown'
 
 export * from './Models/Person'
 
