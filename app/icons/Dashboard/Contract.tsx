37f2c1e5aad0ec0d4a13eca0e27b197473e5957d
sadra <sadrataghavi1383@gmail.com>
2023-07-22 16:22:11 +0330

    refactor(dashboard): icons

--- app/icons/Dashboard/Contract.tsx
+++ app/icons/Dashboard/Contract.tsx
@@ -1,13 +1,14 @@
-import React, { FC } from 'react'
+import React from 'react'
 
-export const ContractSvg: FC = () => (
+export const ContractSvg: Icon = ({ size, ...attr }) => (
     <svg
         stroke='black'
         fill='currentColor'
         strokeWidth='0'
         viewBox='0 0 24 24'
-        height='1em'
-        width='1em'
+        height={size}
+        width={size}
+        {...attr}
         xmlns='http://www.w3.org/2000/svg'
     >
         <g>
