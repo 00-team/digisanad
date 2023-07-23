0325ac238a1847a289560dd6172e2b4a7d9fce49
007 <dr007cc@gmail.com>
2023-07-23 05:17:50 +0330

    fix: update general db wrappers

--- db/general.py
+++ db/general.py
@@ -7,16 +7,20 @@ from .models import GeneralModel, GeneralTable
 
 
 async def general_get() -> GeneralModel:
-    row = await sqlx.fetch_one(select(GeneralTable).limit(1))
+    row = await sqlx.fetch_one(
+        select(GeneralTable)
+        .where(GeneralTable.general_id == 0)
+    )
+
     if row is None:
-        gid = await sqlx.execute(insert(GeneralTable))
-        return GeneralModel(general_id=gid, coin=[])
+        await sqlx.execute(insert(GeneralTable), {'general_id': 0})
+        return GeneralModel(general_id=0, coins={})
 
     return GeneralModel(**row)
 
 
-async def general_update(gid: int, **values: dict):
+async def general_update(**values: dict):
     await sqlx.execute(
-        update(GeneralTable).where(GeneralTable.general_id == gid),
+        update(GeneralTable).where(GeneralTable.general_id == 0),
         values
     )
