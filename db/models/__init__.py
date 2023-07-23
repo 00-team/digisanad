53895111692931235ce911b61a6b3214f2f284c9
007 <dr007cc@gmail.com>
2023-07-23 07:58:59 +0330

    fix: export model_dict

--- db/models/__init__.py
+++ db/models/__init__.py
@@ -1,11 +1,11 @@
-from .common import BaseCoin, BaseTable, NetworkType, metadata
+from .common import BaseCoin, BaseTable, NetworkType, metadata, model_dict
 from .general import GeneralCoin, GeneralModel, GeneralTable
 from .transaction import TransactionModel, TransactionStatus, TransactionTable
 from .user import UserModel, UserTable
 from .wallet import WalletAccount, WalletCoin, WalletModel, WalletTable
 
 __all__ = [
-    'BaseTable', 'metadata', 'NetworkType', 'BaseCoin',
+    'BaseTable', 'metadata', 'NetworkType', 'BaseCoin', 'model_dict',
     'UserTable', 'UserModel',
     'WalletTable', 'WalletModel', 'WalletCoin', 'WalletAccount',
     'GeneralTable', 'GeneralCoin', 'GeneralModel',
