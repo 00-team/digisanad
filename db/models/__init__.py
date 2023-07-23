4cc157f16db0ef036a551e1d543a15f4191a43d6
007 <dr007cc@gmail.com>
2023-07-23 16:49:35 +0330

    fix: export AdminPerms

--- db/models/__init__.py
+++ db/models/__init__.py
@@ -1,12 +1,12 @@
 from .common import BaseCoin, BaseTable, NetworkType, metadata, model_dict
 from .general import GeneralCoin, GeneralModel, GeneralTable
 from .transaction import TransactionModel, TransactionStatus, TransactionTable
-from .user import UserModel, UserTable
+from .user import AdminPerms, UserModel, UserTable
 from .wallet import WalletAccount, WalletCoin, WalletModel, WalletTable
 
 __all__ = [
     'BaseTable', 'metadata', 'NetworkType', 'BaseCoin', 'model_dict',
-    'UserTable', 'UserModel',
+    'UserTable', 'UserModel', 'AdminPerms',
     'WalletTable', 'WalletModel', 'WalletCoin', 'WalletAccount',
     'GeneralTable', 'GeneralCoin', 'GeneralModel',
     'TransactionStatus', 'TransactionModel', 'TransactionTable',
