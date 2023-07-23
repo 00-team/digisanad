011303e96f70badca7e2b2bad90fce3242bca46d
007 <dr007cc@gmail.com>
2023-07-23 17:30:43 +0330

    fix: update message_unseen_count

--- db/message.py
+++ db/message.py
@@ -17,6 +17,9 @@ async def message_add(**values: dict) -> int:
     return await sqlx.execute(insert(MessageTable), values)
 
 
-async def message_unseen_count() -> int:
-    query = 'SELECT COUNT(0) FROM messages WHERE seen is false'
-    return await sqlx.fetch_one(query)[0]
+async def message_unseen_count(user_id: int) -> int:
+    query = '''
+        SELECT COUNT(0) FROM messages
+        WHERE seen IS false AND receiver = :user_id
+    '''
+    return await sqlx.fetch_one(query, {'user_id': user_id})[0]
