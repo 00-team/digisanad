45bfbbe722d9078d6c33578efdeafca4e4cfb315
007 <dr007cc@gmail.com>
2023-07-23 23:36:24 +0330

    feat: add admin add_message

--- api/modules/admin.py
+++ api/modules/admin.py
@@ -1,11 +1,14 @@
 
 
 from fastapi import APIRouter, Request
+from pydantic import BaseModel
 
 from db.general import general_get
+from db.message import message_add
 from db.models import AdminPerms as AP
-from db.models import GeneralModel, UserModel
+from db.models import GeneralModel, MessageLevel, UserModel
 from deps import admin_required
+from shared.tools import utc_now
 
 router = APIRouter(
     prefix='/admins',
@@ -20,3 +23,19 @@ async def get_general(request: Request):
     user.admin_assert(AP.V_GENERAL)
 
     return await general_get()
+
+
+class MessageAddBody(BaseModel):
+    receiver: int
+    text: str
+    level: MessageLevel = MessageLevel.INFO
+
+
+@router.post('/message/', response_model=int)
+async def add_message(request: Request, body: MessageAddBody):
+    return await message_add(
+        text=body.text,
+        receiver=body.receiver,
+        timestamp=utc_now(),
+        level=body.level
+    )
