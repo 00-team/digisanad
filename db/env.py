3bd82db20c13c84a7e78098f3d93a403cf1bbca1
007 <dr007cc@gmail.com>
2023-07-23 04:51:26 +0330

    fix: use SQL_URL in db/env

--- db/env.py
+++ db/env.py
@@ -4,10 +4,10 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 
 from db.models import metadata
-from shared import settings
+from shared import SQL_URL
 
 config = context.config
-config.set_main_option('sqlalchemy.url', settings.sql_url)
+config.set_main_option('sqlalchemy.url', SQL_URL)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
