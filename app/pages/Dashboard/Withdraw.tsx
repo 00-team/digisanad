68a1892d726ba9c44ee4d0cd2c35103e10dfc590
sadra <sadrataghavi1383@gmail.com>
2023-07-23 18:18:56 +0330

    feat(dashboard): new section

---
+++ app/pages/Dashboard/Withdraw.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+import './style/withdraw.scss'
+
+const Withdraw: FC = () => {
+    return <section className='withdraw-container'>Withdraw</section>
+}
+
+export default Withdraw
