76b6440f4dfa0779e4b94c5e2cfd1893df2bfb3a
sadra <sadrataghavi1383@gmail.com>
2023-07-24 14:22:19 +0330

    feat(dashboard): transactions update

--- app/pages/Dashboard/Transactions.tsx
+++ app/pages/Dashboard/Transactions.tsx
@@ -1,13 +1,72 @@
-import React, { FC } from 'react'
+import React, { FC, HTMLAttributes } from 'react'
+
+import { CallenderSvg, CoinSvg, UnknownSvg } from 'icons'
 
 import './style/transactions.scss'
 
 const Transactions: FC = () => {
     return (
         <section className='transactions-container'>
-            <h2 className='section-header section_title'>کیف پول</h2>
-            <div className='transactions-wrapper'></div>
+            <h2 className='section-header section_title'>تراکنش های من </h2>
+            <div className='transactions-wrapper'>
+                <TransactionCard type='deposit' />
+                <TransactionCard type='withdraw' />
+                <TransactionCard type='deposit' />
+                <TransactionCard type='withdraw' />
+                <TransactionCard type='deposit' />
+                <TransactionCard type='withdraw' />
+            </div>
         </section>
+    )
+}
+
+interface TransactionCardProps extends HTMLAttributes<HTMLDivElement> {
+    type: 'deposit' | 'withdraw'
+}
+
+const TransactionCard: FC<TransactionCardProps> = ({ type, ...attr }) => {
+    type titleTypes = {
+        [k in typeof type]: string
+    }
+    const titles: titleTypes = {
+        deposit: 'واریز',
+        withdraw: 'برداشت',
+    }
+
+    return (
+        <div className={`transaction-card ${type}`} {...attr}>
+            <h3 className='card-title title'>{titles[type]}</h3>
+            <div className='datas'>
+                <div className='row title_small'>
+                    <div className='holder-wrapper'>
+                        <div className='icon'>
+                            <CallenderSvg size={25} />
+                        </div>
+                        <div className='holder '>تاریخ</div>
+                    </div>
+                    <div className='data'>1400/9/9</div>
+                </div>
+                <div className='row title_small'>
+                    <div className='holder-wrapper'>
+                        <div className='icon'>
+                            <CoinSvg size={25} />
+                        </div>
+                        <div className='holder '>مقدار</div>
+                    </div>
+                    <div className='data'>0.1254</div>
+                </div>
+                <div className='row title_small'>
+                    <div className='holder-wrapper'>
+                        <div className='icon'>
+                            <UnknownSvg size={25} />
+                        </div>
+                        <div className='holder '>کوین</div>
+                    </div>
+                    <div className='data'>اتریوم</div>
+                </div>
+            </div>
+            <div></div>
+        </div>
     )
 }
 
