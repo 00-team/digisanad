a6f48ee52c6e8ec723a47a2178c2cbd65eb455f2
sadra <sadrataghavi1383@gmail.com>
2023-07-25 13:35:55 +0330

    feat(withdraw)

--- app/pages/Dashboard/Withdraw.tsx
+++ app/pages/Dashboard/Withdraw.tsx
@@ -1,9 +1,26 @@
 import React, { FC } from 'react'
 
+import { WalletSvg } from 'icons'
+
 import './style/withdraw.scss'
 
 const Withdraw: FC = () => {
-    return <section className='withdraw-container'>Withdraw</section>
+    return (
+        <section className='withdraw-container'>
+            <h2 className='section-header section_title'>برداشت موجودی </h2>
+            <div className='withdraw-wrapper'>
+                <div className='current-container'>
+                    <div className='wallet-icon'>
+                        <WalletSvg size={35} />
+                    </div>
+                    <div className='wallet-data title'>
+                        <div className='holder'>موجودی شما:</div>
+                        <div className='data'>0.124124</div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Withdraw
