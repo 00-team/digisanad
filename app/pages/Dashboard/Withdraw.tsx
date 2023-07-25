aacff7ec0d855e163f23b73609d70a7778d0e5a1
sadra <sadrataghavi1383@gmail.com>
2023-07-25 16:05:21 +0330

    feat(withdraw)

--- app/pages/Dashboard/Withdraw.tsx
+++ app/pages/Dashboard/Withdraw.tsx
@@ -1,12 +1,44 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 
-import { CoinSvg, WalletSvg } from 'icons'
+import { get_wallet } from 'api'
+import { CoinSvg, UnknownSvg, WalletSvg } from 'icons'
 
-import { Submit } from 'components'
+import { useAtom, useAtomValue } from 'jotai'
+import { TokenAtom, WalletAtom } from 'state'
+
+import { Loading, Option, Select, Submit } from 'components'
 
 import './style/withdraw.scss'
 
 const Withdraw: FC = () => {
+    const [wallet, setWallet] = useAtom(WalletAtom)
+    const token = useAtomValue(TokenAtom)
+
+    useEffect(() => {
+        // this should never be reached
+        // but anyway
+        if (!token) return
+
+        if (wallet == null || wallet.next_update < Date.now()) {
+            get_wallet(token).then(data => setWallet(data))
+            return
+        }
+    }, [])
+
+    useEffect(() => {
+        console.log(wallet)
+    }, [wallet])
+
+    if (wallet == null) {
+        return (
+            <section id='wallet' className='withdraw-container'>
+                <h2 className='section-header section_title'>برداشت موجودی </h2>
+                <div className='withdraw-wrapper'>
+                    <Loading style={{ height: '50vh' }} />
+                </div>
+            </section>
+        )
+    }
     return (
         <section className='withdraw-container'>
             <h2 className='section-header section_title'>برداشت موجودی </h2>
@@ -17,7 +49,7 @@ const Withdraw: FC = () => {
                     </div>
                     <div className='wallet-data title'>
                         <div className='holder'>موجودی شما:</div>
-                        <div className='data'>0.124124</div>
+                        <div className='data'>{wallet.coins[0]?.in_wallet}</div>
                     </div>
                 </div>
                 <div className='wallet-options'>
@@ -32,6 +64,42 @@ const Withdraw: FC = () => {
                             maxLength={1024}
                         />
                     </div>
+                    <div className='wallet-option coin-options title_smaller'>
+                        <h3 className=' option-title'>
+                            <UnknownSvg size={25} />
+                            <div className='holder'>کوین برداشتی</div>
+                        </h3>
+                        <Select
+                            defaultOpt={{
+                                value: wallet.coins[0]?.name,
+                                label: wallet.coins[0]?.display || '',
+                            }}
+                            options={wallet.coins.map(
+                                (coin): Option => ({
+                                    value: coin.name,
+                                    label: coin.display,
+                                })
+                            )}
+                        />
+                    </div>
+                    <div className='wallet-option coin-options title_smaller'>
+                        <h3 className=' option-title'>
+                            <UnknownSvg size={25} />
+                            <div className='holder'>شبکه</div>
+                        </h3>
+                        <Select
+                            defaultOpt={{
+                                value: wallet.coins[0]?.network,
+                                label: wallet.coins[0]?.network || '',
+                            }}
+                            options={wallet.coins.map(
+                                (coin): Option => ({
+                                    value: coin.network,
+                                    label: coin.network,
+                                })
+                            )}
+                        />
+                    </div>
                     <div className='wallet-option title_smaller'>
                         <h3 className=' option-title'>
                             <CoinSvg size={25} />
@@ -41,6 +109,7 @@ const Withdraw: FC = () => {
                             className='option-input'
                             type='text'
                             maxLength={1024}
+                            placeholder={`حداکثر: ${wallet.coins[0]?.in_wallet}`}
                         />
                     </div>
                 </div>
