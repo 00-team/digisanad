ad0e0f4d887418e50741c6d5b0c8a907ae078218
sadra <sadrataghavi1383@gmail.com>
2023-07-29 16:04:09 +0330

    feat(dashboard): notifications

---
+++ app/pages/Dashboard/Notifications.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+import './style/notifications.scss'
+
+const Notifications: FC = () => {
+    return <div className='notifications-container'>Notifications</div>
+}
+
+export default Notifications
