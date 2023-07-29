9adfc7bfef7b74f56759735280b03f5497eeef53
sadra <sadrataghavi1383@gmail.com>
2023-07-29 16:22:14 +0330

    feat(notif)

--- app/pages/Dashboard/Notifications.tsx
+++ app/pages/Dashboard/Notifications.tsx
@@ -1,9 +1,17 @@
 import React, { FC } from 'react'
 
+import { NotificationSvg } from 'icons'
+
 import './style/notifications.scss'
 
 const Notifications: FC = () => {
-    return <div className='notifications-container'>Notifications</div>
+    return (
+        <div className='notifications-container'>
+            <button className='notification-icon'>
+                <NotificationSvg size={innerWidth >= 1024 ? 40 : 20} />
+            </button>
+        </div>
+    )
 }
 
 export default Notifications
