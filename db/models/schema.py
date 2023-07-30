b616a5e726ee116aa7a3f06defaaf5f9b0dc4bd3
007 <dr007cc@gmail.com>
2023-07-30 12:18:24 +0330

    feat: add optinal prop for schemas

--- db/models/schema.py
+++ db/models/schema.py
@@ -26,6 +26,7 @@ class BaseField(BaseModel):
     description: str = Field(
         None, description='optinal description of the field'
     )
+    optinal: bool = False
 
 
 class UserField(BaseField):
