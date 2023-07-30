ac475308a69feccd082ab4d1c207b6ca453eb2f0
sadra <sadrataghavi1383@gmail.com>
2023-07-30 16:41:18 +0330

    feat(notif)

--- app/pages/Dashboard/Notifications.tsx
+++ app/pages/Dashboard/Notifications.tsx
@@ -3,10 +3,10 @@ import React, { FC, useEffect, useState } from 'react'
 import { C } from '@00-team/utils'
 
 import { get_messages } from 'api'
-import { NotificationSvg } from 'icons'
+import { InfoSvg, NotificationSvg, WarningSvg } from 'icons'
 
 import { useAtom, useAtomValue } from 'jotai'
-import { MessagesAtom, TokenAtom } from 'state'
+import { MessageModel, MessagesAtom, TokenAtom } from 'state'
 
 import './style/notifications.scss'
 
@@ -41,20 +41,40 @@ const Notifications: FC = () => {
                 <NotificationSvg size={innerWidth >= 1024 ? 40 : 30} />
             </button>
             <div className={`notifications-wrapper ${C(Open)}`}>
-                <NotifMessage />
-                <NotifMessage />
-                <NotifMessage />
+                <NotifMessage level='info' />
+                <NotifMessage level='notification' />
+                <NotifMessage level='urgent' />
             </div>
         </div>
     )
 }
 
-interface NotifMessageProps {}
+interface NotifMessageProps extends MessageModel {}
 
-const NotifMessage: FC<NotifMessageProps> = () => {
+const NotifMessage: FC<Pick<NotifMessageProps, 'level'>> = ({ level }) => {
+    type levels = {
+        [k in typeof level]: string
+    }
+
+    const levels_title: levels = {
+        info: 'اطلاعیه',
+        notification: '',
+        urgent: 'مهم',
+    } as const
+
     return (
-        <div className='notif-container'>
+        <div className={`notif-container ${level}`}>
             <div className='has-seen'></div>
+
+            {level !== 'notification' && (
+                <div className={`notif-level ${level}`}>
+                    {level === 'info' && <InfoSvg fill='#5199ff' size={30} />}
+                    {level === 'urgent' && (
+                        <WarningSvg fill='#e20338' size={30} />
+                    )}
+                    <p className='title_small'>{levels_title[level]}</p>
+                </div>
+            )}
         </div>
     )
 }
