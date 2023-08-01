efc9b369fd0f738b19cfa6f65ebcccd69a01035f
sadra <sadrataghavi1383@gmail.com>
2023-08-01 15:58:16 +0330

    feat(admin):: sidebar init

---
+++ app/pages/Admin/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react'
+
+import './style/admin.scss'
+
+const Admin: FC = () => {
+    return (
+        <main className='admin-container'>
+            <aside className='admin-sidebar'></aside>
+        </main>
+    )
+}
+
+export default Admin
