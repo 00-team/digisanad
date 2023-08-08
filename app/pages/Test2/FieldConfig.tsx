a404026eec08cf094de526e55077c27acdb92dae
007 <dr007cc@gmail.com>
2023-08-09 02:08:07 +0330

    feat: add optinal input and description textarea

--- app/pages/Test2/FieldConfig.tsx
+++ app/pages/Test2/FieldConfig.tsx
@@ -10,6 +10,20 @@ type FieldConfigProps = {
 const FieldConfig: FC<FieldConfigProps> = ({ field, update }) => {
     return (
         <div className='field-config'>
+            <span>type: {field.type}</span>
+            <span>uid: {field.uid}</span>
+            <div className='row'>
+                <input
+                    id='fc_optinal'
+                    type='checkbox'
+                    checked={field.optinal}
+                    onChange={e => {
+                        field.optinal = e.currentTarget.checked
+                        update()
+                    }}
+                />
+                <label htmlFor='fc_optinal'>Optinal</label>
+            </div>
             <input
                 type='text'
                 value={field.title}
@@ -19,7 +33,14 @@ const FieldConfig: FC<FieldConfigProps> = ({ field, update }) => {
                 }}
                 placeholder='title'
             />
-            {field.uid} - {field.title} - {field.type}
+            <textarea
+                value={field.description || ''}
+                onInput={e => {
+                    field.description = e.currentTarget.value
+                    update()
+                }}
+                placeholder='description'
+            ></textarea>
         </div>
     )
 }
