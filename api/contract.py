e4cd2787f00d9fb98979f1a3f91b3abba55414c1
007 <dr007cc@gmail.com>
2023-08-11 01:00:15 +0330

    feat: add contract create api

--- api/contract.py
+++ api/contract.py
@@ -1,16 +1,18 @@
 
 import json
-from typing import Literal
 
 from fastapi import APIRouter, Request
 from pydantic import BaseModel
 
-from db.models import ContractModel, ContractTable, ContractUserModel
+from db.contract import contract_add, contract_user_add
+from db.models import ContractModel, ContractStage, ContractTable
 from db.models import ContractUserTable, UserModel, UserPublic
 from db.user import user_public
 from deps import user_required
 from shared import settings, sqlx
 from shared.errors import bad_id
+from shared.models import IDModel, OkModel
+from shared.tools import utc_now
 
 router = APIRouter(
     prefix='/contracts',
@@ -50,3 +52,24 @@ async def get_contracts(request: Request, page: int = 0):
         result.append(ContractModel(**args))
 
     return result
+
+
+class CreateBody(BaseModel):
+    data: dict
+
+
+@router.post('/', response_model=IDModel)
+async def create(request: Request, body: CreateBody):
+    user: UserModel = request.state.user
+
+    contract_id = await contract_add(
+        creator=user.user_id,
+        stage=ContractStage.DRAFT,
+        data=body.data,
+        start_date=utc_now(),
+        finish_date=0
+    )
+
+    await contract_user_add(contract_id, user.user_id)
+
+    return {'id': contract_id}
