81e503359e68eac483d6e5ce875ea84de3848381
007 <dr007cc@gmail.com>
2023-08-10 17:29:49 +0330

    fix: update get_schemas api

--- api/modules/schema.py
+++ api/modules/schema.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Request
 
 from db.models import SchemaModel, SchemaTable, UserModel
 from deps import user_required
+from shared import settings, sqlx
 
 router = APIRouter(
     prefix='/schemas',
@@ -13,7 +14,23 @@ router = APIRouter(
 
 
 @router.get('/', response_model=list[SchemaModel])
-async def get_schemas(request: Request, page: int = 0):
+async def get_schemas(request: Request, page: int = 0, mine: bool = False):
     user: UserModel = request.state.user
+    if mine:
+        creator = user.user_id
+    else:
+        creator = -1
 
-    return []
+    rows = await sqlx.fetch_all(
+        f'''
+        SELECT * FROM schemas WHERE creator = :creator
+        ORDER BY schema_id DESC
+        LIMIT {settings.page_size} OFFSET :skip
+        ''',
+        {
+            'skip': page * settings.page_size,
+            'creator': creator
+        }
+    )
+
+    return [SchemaModel(**r) for r in rows]
