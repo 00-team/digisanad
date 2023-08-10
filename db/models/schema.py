d676b5e414477a05b7f54593fd69b17fada5d3bf
007 <dr007cc@gmail.com>
2023-08-10 17:25:31 +0330

    feat: add creator column for schema

--- db/models/schema.py
+++ db/models/schema.py
@@ -1,9 +1,7 @@
 
-from enum import Enum
-from typing import Literal
 
-from pydantic import BaseModel, Field, constr
-from sqlalchemy import JSON, Boolean, Column, Integer, String
+from pydantic import BaseModel
+from sqlalchemy import JSON, Boolean, Column, Integer, String, text
 
 from .common import BaseTable
 
@@ -19,6 +17,7 @@ class SchemaTable(BaseTable):
     title = Column(String, nullable=False)
     description = Column(String)
     data = Column(JSON, nullable=False, server_default='{}')
+    creator = Column(Integer, nullable=False, server_default=text('-1'))
 
 
 class SchemaModel(BaseModel):
@@ -27,3 +26,4 @@ class SchemaModel(BaseModel):
     title: str
     description: str | None = None
     data: dict
+    creator: int
