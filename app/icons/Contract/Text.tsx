420cc4e257665d505b9d1cd0010f7f34be71d615
sadra <sadrataghavi1383@gmail.com>
2023-08-13 16:15:11 +0330

    feat(contract): icons

---
+++ app/icons/Contract/Text.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const TextSvg: Icon = ({ size, ...attr }) => {
+    return (
+        <svg
+            stroke='currentColor'
+            fill='currentColor'
+            stroke-width='0'
+            viewBox='0 0 24 24'
+            height={size}
+            width={size}
+            {...attr}
+            xmlns='http://www.w3.org/2000/svg'
+        >
+            <path d='M5 8h2V6h3.252L7.68 18H5v2h8v-2h-2.252L13.32 6H17v2h2V4H5z'></path>
+        </svg>
+    )
+}
