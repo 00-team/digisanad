6a460dffc217a90af2c64b53db4f4ccd1790c898
sadra <sadrataghavi1383@gmail.com>
2023-09-06 19:04:13 +0330

    feat(userslist)

---
+++ app/pages/Admin/usersList.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+import './style/userlist.scss'
+
+const UsersList: FC = () => {
+    return <section className='users-container'>UsersList</section>
+}
+
+export default UsersList
