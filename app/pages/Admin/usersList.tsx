c3823befc45fef11a106af3dccc7ed4493c57255
sadra <sadrataghavi1383@gmail.com>
2023-09-06 19:36:12 +0330

    feat(admin)

--- app/pages/Admin/usersList.tsx
+++ app/pages/Admin/usersList.tsx
@@ -1,8 +1,37 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
+
+import axios from 'axios'
+
+import { useAtomValue } from 'jotai'
+import { TokenAtom, UserModel } from 'state'
 
 import './style/userlist.scss'
 
 const UsersList: FC = () => {
+    const token = useAtomValue(TokenAtom)
+
+    const [users, setusers] = useState<UserModel[] | null>(null)
+
+    const fetch_users = async () => {
+        const response = await axios.post(
+            '/api/admin/users/',
+            {},
+            {
+                headers: {
+                    Authorization: 'Bearer ' + token,
+                },
+            }
+        )
+        setusers(response.data)
+    }
+
+    useEffect(() => {
+        fetch_users()
+    }, [])
+    useEffect(() => {
+        console.log(users)
+    }, [users])
+
     return <section className='users-container'>UsersList</section>
 }
 
