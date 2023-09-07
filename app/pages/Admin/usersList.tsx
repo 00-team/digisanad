919e8cd34a3cbe5b602681ec4f1ae9c3b3a40487
sadra <sadrataghavi1383@gmail.com>
2023-09-07 14:12:00 +0330

    feat(userlist)

--- app/pages/Admin/usersList.tsx
+++ app/pages/Admin/usersList.tsx
@@ -32,7 +32,36 @@ const UsersList: FC = () => {
         console.log(users)
     }, [users])
 
-    return <section className='users-container'>UsersList</section>
+    return (
+        <section className='users-container'>
+            <h3 className='section_title'>کاربران سایت</h3>
+            <div className='users-wrapper'>
+                <table>
+                    <thead className='title_small'>
+                        <th className='id'>شماره</th>
+                        <th className='name'>نام</th>
+                        <th className='lname'>نام خانوادگی</th>
+                        <th className='phone'>شماره تلفن</th>
+                    </thead>
+                    <tbody className='title_smaller'>
+                        {users &&
+                            users.map(
+                                ({ first_name, last_name, user_id, phone }) => {
+                                    return (
+                                        <tr>
+                                            <td>{user_id}</td>
+                                            <td>{first_name}</td>
+                                            <td>{last_name}</td>
+                                            <td>{phone}</td>
+                                        </tr>
+                                    )
+                                }
+                            )}
+                    </tbody>
+                </table>
+            </div>
+        </section>
+    )
 }
 
 export default UsersList
