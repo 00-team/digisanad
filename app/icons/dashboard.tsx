fdee5a9b10022aeceece35b82b51e88e6d40757f
sadra <sadrataghavi1383@gmail.com>
2023-09-11 16:48:41 +0330

    feat(app)

--- app/icons/dashboard.tsx
+++ app/icons/dashboard.tsx
@@ -184,6 +184,22 @@ export const ContractIcon: Icon = ({ size = '24', ...attr }) => (
     </svg>
 )
 
+export const PrintIcon: Icon = ({ size = '24', ...attr }) => (
+    <svg
+        stroke='currentColor'
+        fill='currentColor'
+        stroke-width='0'
+        viewBox='0 0 16 16'
+        height={size}
+        width={size}
+        xmlns='http://www.w3.org/2000/svg'
+        {...attr}
+    >
+        <path d='M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5zm6 8H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z'></path>
+        <path d='M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z'></path>
+    </svg>
+)
+
 export const CoinIcon: Icon = ({ size = '24', ...attr }) => (
     <svg
         stroke='currentColor'
